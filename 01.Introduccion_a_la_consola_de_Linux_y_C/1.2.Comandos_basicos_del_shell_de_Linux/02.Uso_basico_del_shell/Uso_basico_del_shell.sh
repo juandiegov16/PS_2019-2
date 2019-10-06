6f3e39252e9d90debf4a74e152937bebaf153b3b
Juandi Vallejo <juadival@espol.edu.ec>
2019-10-05 20:43:26 -0500

    Finalizada actividad "Uso básico del shell"

---
+++ 01.Introduccion_a_la_consola_de_Linux_y_C/1.2.Comandos_basicos_del_shell_de_Linux/02.Uso_basico_del_shell/Uso_basico_del_shell.sh
@@ -0,0 +1,11 @@
+cd
+
+mkdir -p logs/2019
+
+cd logs/2019
+
+pwd
+
+touch bitacoraOCT2019.log bitacoraNOV2019.log bitacoraDIC2019.log bitacoraENE2020.log bitacoraFEB2020.log bitacoraMAR2020.log
+
+mv *2020.log ..
