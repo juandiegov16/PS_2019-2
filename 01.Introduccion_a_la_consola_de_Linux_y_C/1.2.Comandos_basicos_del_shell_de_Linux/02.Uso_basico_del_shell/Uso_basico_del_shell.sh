f8a660361dfd8be2c3c2fe1a87003c8614d15a57
Juandi Vallejo <juadival@espol.edu.ec>
2019-10-06 20:14:02 -0500

    Uso de brace expansion en Uso_basico_del_shell.sh

--- 01.Introduccion_a_la_consola_de_Linux_y_C/1.2.Comandos_basicos_del_shell_de_Linux/02.Uso_basico_del_shell/Uso_basico_del_shell.sh
+++ 01.Introduccion_a_la_consola_de_Linux_y_C/1.2.Comandos_basicos_del_shell_de_Linux/02.Uso_basico_del_shell/Uso_basico_del_shell.sh
@@ -6,6 +6,6 @@ cd logs/2019
 
 pwd
 
-touch bitacoraOCT2019.log bitacoraNOV2019.log bitacoraDIC2019.log bitacoraENE2020.log bitacoraFEB2020.log bitacoraMAR2020.log
+touch bitacora{OCT,NOV,DEC}2019.log bitacora{ENE,FEB,MAR}2019.log
 
 mv *2020.log ..
