cff891a71c644bfab3f1dd3f8ef53591286f6055
Juandi Vallejo <juadival@espol.edu.ec>
2019-10-07 12:05:16 -0500

    UPD typo

--- 01.Introduccion_a_la_consola_de_Linux_y_C/1.2.Comandos_basicos_del_shell_de_Linux/02.Uso_basico_del_shell/Uso_basico_del_shell.sh
+++ 01.Introduccion_a_la_consola_de_Linux_y_C/1.2.Comandos_basicos_del_shell_de_Linux/02.Uso_basico_del_shell/Uso_basico_del_shell.sh
@@ -4,8 +4,6 @@ mkdir -p logs/2019
 
 cd logs/2019
 
-pwd
-
-touch bitacora{OCT,NOV,DEC}2019.log bitacora{ENE,FEB,MAR}2019.log
+touch bitacora{OCT,NOV,DIC}2019.log bitacora{ENE,FEB,MAR}2020.log
 
 mv *2020.log ..
